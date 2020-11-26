48c7c1dd3f8c39ae0e12f9ba38d4386980bc001c
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-11-26 10:27:50 +0200

    Changed helfi base config module dependency to helfi platform config.

--- tools/make/project/install.mk
+++ tools/make/project/install.mk
@@ -5,4 +5,4 @@ endif
 PHONY += drush-enable-modules
 drush-enable-modules: ## Enable modules and base configurations.
 	$(call step,Install base configurations...)
-	$(call drush_on_docker,en -y features helfi_base_config)
+	$(call drush_on_docker,en -y features helfi_platform_config)
