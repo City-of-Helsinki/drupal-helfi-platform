5fb3fb41f80c9f2b7137f6e750bc5f51754af66f
tuutti <tuutti@iki.fi>
2021-03-08 11:35:32 +0200

    Skip commit script if STDIN is empty

--- tools/make/project/git.mk
+++ tools/make/project/git.mk
@@ -1,3 +1,3 @@
 PHONY += copy-commit-message-script
 copy-commit-message-script:
-	@$(foreach name,$(shell find public/modules/custom public/themes/custom -type d -name ".git" -print0 | xargs -0 dirname ) .,cp tools/commit-msg $(name)/.git/hooks;)
+	@$(foreach name,$(shell find public/modules/custom public/themes/custom -type d -name ".git" -print0 | xargs -0 --no-run-if-empty dirname ) .,cp tools/commit-msg $(name)/.git/hooks;)
