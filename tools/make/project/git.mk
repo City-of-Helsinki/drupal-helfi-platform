db2123ed10fcd9a3d33ff155855df69a1ac52636
tuutti <tuutti@iki.fi>
2021-03-12 15:45:27 +0200

    Fixed copy commit script for OSX

--- tools/make/project/git.mk
+++ tools/make/project/git.mk
@@ -1,3 +1,3 @@
 PHONY += copy-commit-message-script
 copy-commit-message-script:
-	@$(foreach name,$(shell find public/modules/custom public/themes/custom -type d -name ".git" -print0 | xargs -0 dirname 2> /dev/null ) .,cp tools/commit-msg $(name)/.git/hooks;)
+	@$(foreach name,$(shell find public/modules/custom public/themes/custom -type d -name ".git" -exec dirname {} \; 2> /dev/null ) .,cp tools/commit-msg $(name)/.git/hooks;)
