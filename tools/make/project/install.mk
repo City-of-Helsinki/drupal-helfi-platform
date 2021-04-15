ee3c5e5071a63cb0376bdfe0b5be16478a5d89ad
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2021-04-15 13:15:04 +0300

    Modified the translation import command when installing a site.

--- tools/make/project/install.mk
+++ tools/make/project/install.mk
@@ -1,6 +1,8 @@
 ifeq ($(DRUPAL_CONF_EXISTS)$(DRUPAL_VERSION),no8)
-    DRUPAL_NEW_TARGETS := up build drush-si drush-enable-modules drush-locale-import drush-uli
-    DRUPAL_POST_INSTALL_TARGETS := drush-updb drush-cim drush-locale-import drush-uli
+    DRUPAL_NEW_TARGETS := up build drush-si drush-enable-modules drush-uli
+endif
+ifeq ($(DRUPAL_VERSION),8)
+    DRUPAL_POST_INSTALL_TARGETS := drush-updb drush-cim drush-locale-update drush-uli
 endif
 
 PHONY += drush-enable-modules
@@ -8,7 +10,8 @@ drush-enable-modules: ## Enable modules and base configurations.
 	$(call step,Install base configurations...)
 	$(call drush_on_docker,en -y helfi_platform_config helfi_base_config)
 
-PHONY += drush-locale-import
-drush-locale-import: ## Import locale PO files
-	$(call step,Import locale PO files...)
-	$(call drush_on_docker,helfi:locale-import helfi_platform_config)
+PHONY += drush-locale-update
+drush-locale-update: ## Update translations.
+	$(call step,Install base configurations...)
+	$(call drush_on_docker,state:set locale.translation_last_checked 0)
+	$(call drush_on_docker,locale:update)
