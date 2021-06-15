a2b571f1ea3ea75e8d3d86d304e6a34d54509322
tuutti <tuutti@iki.fi>
2021-06-15 09:46:29 +0300

    Copy commit-msg script to contrib folder as well

--- tools/make/project/git.mk
+++ tools/make/project/git.mk
@@ -1,3 +1,3 @@
 PHONY += copy-commit-message-script
 copy-commit-message-script:
-	@$(foreach name,$(shell find public/modules/custom public/themes/custom -type d -name ".git" -exec dirname {} \; 2> /dev/null ) .,cp tools/commit-msg $(name)/.git/hooks;)
+	@$(foreach name,$(shell find public/modules/custom public/themes/custom public/modules/contrib public/themes/contrib -type d -name ".git" -exec dirname {} \; 2> /dev/null ) .,cp tools/commit-msg $(name)/.git/hooks;)
