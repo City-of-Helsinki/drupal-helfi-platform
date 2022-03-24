143115e911744a034da087cd281955eda46a8fa0
tuutti <tuutti@iki.fi>
2022-03-24 07:18:57 +0200

    Post install fixes

--- tools/make/project/spell.mk
+++ tools/make/project/spell.mk
@@ -8,7 +8,8 @@ ifeq ($(UNAME_S),Darwin)
 else
 	@sed -i '/composer.lock/d' .gitignore
 endif
-	@rm -rf .env-e docs LICENSE.md README.md tools/make/project/spell.mk
+	@rm documentation/images/* documentation/* CHANGELOG.md LICENSE.md README.md tools/make/project/spell.mk
+	@rmdir documentation/images documentation/
 	@mv README.project.md README.md
 	@$(MAKE) self-update
 	@composer config --unset scripts.post-create-project-cmd
