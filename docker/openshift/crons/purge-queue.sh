cfe1433a63a1eea6fc392a6b27cfff43717f5105
tuutti <tuutti@iki.fi>
2022-04-05 09:25:06 +0300

    Make sure queue has items before attempting to flush queue

--- docker/openshift/crons/purge-queue.sh
+++ docker/openshift/crons/purge-queue.sh
@@ -2,10 +2,21 @@
 
 echo "Running purge queue: $(date)"
 
+function has_items {
+  NUM_ITEMS=$(drush p:queue-stats --format=json | jq .number_of_items)
+
+  if [ "$NUM_ITEMS" -gt "0" ]; then
+    return 0
+  fi
+  return 1
+}
+
 while true
 do
-  echo "Flushing purge queue: $(date)"
-  drush p:queue-work --no-interaction -q --finish
+  if has_items; then
+    echo "Flushing purge queue: $(date)"
+    drush p:queue-work --no-interaction -q --finish
+  fi
   # Sleep for 60 seconds.
   sleep 60
 done
