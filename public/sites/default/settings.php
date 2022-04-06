8bbf244d62740fcb5f3f668b27b21be99489ac6b
tuutti <tuutti@iki.fi>
2022-04-06 06:51:04 +0300

    Always allow localhost, ignore phpunit results

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -52,9 +52,10 @@ $settings['siteimprove_id'] = getenv('SITEIMPROVE_ID');
 
 // Drupal route(s).
 $routes = (getenv('DRUPAL_ROUTES')) ? explode(',', getenv('DRUPAL_ROUTES')) : [];
+$routes[] = 'http://127.0.0.1';
 
 foreach ($routes as $route) {
-  $hosts[] = $host = parse_url($route)['host'];
+  $host = parse_url($route)['host'];
   $trusted_host = str_replace('.', '\.', $host);
   $settings['trusted_host_patterns'][] = '^' . $trusted_host . '$';
 }
