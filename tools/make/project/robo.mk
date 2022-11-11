2856d869d792e52ffc75ba66931fc1f51afa8595
tuutti <tuutti@iki.fi>
2022-11-11 09:49:02 +0200

    Added missing -y flag

--- tools/make/project/robo.mk
+++ tools/make/project/robo.mk
@@ -31,10 +31,9 @@ PHONY += install-drupal-ci-clean
 install-drupal-ci:
 	$(call docker_run_ci,app,drush si minimal -y)
 	$(call docker_run_ci,app,drush cr)
-	$(call docker_run_ci,app,ls -la)
 	$(call docker_run_ci,app,composer config repositories.5 path /app/helfi_platform_config)
 	$(call docker_run_ci,app,composer require drupal/helfi_platform_config -W)
-	$(call docker_run_ci,app,set -e && drush en helfi_platform_config && for d in /app/public/modules/contrib/helfi_platform_config/helfi_features/*; do drush en -y $$(echo "$$d" | awk -F/ '{print $$NF}'); done)
+	$(call docker_run_ci,app,set -e && drush en helfi_platform_config -y && for d in /app/public/modules/contrib/helfi_platform_config/helfi_features/*; do drush en -y $$(echo "$$d" | awk -F/ '{print $$NF}'); done)
 
 PHONY += install-drupal
 install-drupal:
