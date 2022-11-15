a23ef34c59a91ebc5db041b5f0cf48d54bac6024
tuutti <tuutti@iki.fi>
2022-11-15 05:48:02 +0200

    Fixed theme enable command

--- tools/make/project/robo.mk
+++ tools/make/project/robo.mk
@@ -34,7 +34,7 @@ install-drupal-ci:
 	$(call docker_run_ci,app,composer config repositories.5 path /app/helfi_platform_config)
 	$(call docker_run_ci,app,composer require drupal/helfi_platform_config -W)
 	$(call docker_run_ci,app,set -e && drush en helfi_platform_config -y && for d in /app/public/modules/contrib/helfi_platform_config/helfi_features/*; do drush en -y \$$(basename "\$$d"); done)
-	$(call docker_run_ci,app,drush enable:theme hdbt)
+	$(call docker_run_ci,app,drush theme:enable hdbt && drush config:set system.theme admin hdbt_admin -y && drush config:set system.theme default hdbt -y)
 	$(call docker_run_ci,app,drush state:set locale.translation_last_checked 0)
 	$(call docker_run_ci,app,drush locale:check)
 	$(call docker_run_ci,app,drush locale:update)
