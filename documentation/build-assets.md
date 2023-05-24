0e664a9e111be4b3dbf900d96cfa3592c978c1e4
tuutti <tuutti@iki.fi>
2023-05-24 08:30:22 +0300

    UHF-8341: Basic make commands, documentation

--- documentation/build-assets.md
+++ documentation/build-assets.md
@@ -16,3 +16,37 @@ docker compose exec -w /app/public/themes/custom/hdbt_subtheme node npm run buil
 # Run 'npm run dev' (Starts SCSS/JS watcher)
 docker compose exec -w /app/public/themes/custom/hdbt_subtheme node npm run dev
 ```
+
+Start an interactive shell: `docker compose exec node sh` or `make node-shell`.
+
+## Make commands
+
+The following `make` commands are included by default:
+
+### Common
+```bash
+make node-shell # Login to node container. Same as `make shell`, but for node container.
+```
+
+### HDBT Subtheme
+```bash
+make install-hdbt-subtheme # Installs NPM dependencies inside hdbt_subtheme folder (`npm install`).
+make build-hdbt-subtheme # Builds SCSS/JS assets inside hdbt_subtheme folder (`npm run build`).
+make watch-hdbt-subtheme # Starts SCSS/JS watcher inside hdbt_subtheme folder (`npm run dev`).
+```
+
+### HDBT theme
+```bash
+make install-hdbt # Installs NPM dependencies inside hdbt folder (`npm install`).
+make build-hdbt # Builds SCSS/JS assets inside hdbt folder (`npm run build`).
+make watch-hdbt # Starts SCSS/JS watcher inside hdbt folder (`npm run dev`).
+```
+
+### HDBT Admin theme
+```bash
+make install-hdbt-admin # Installs NPM dependencies inside hdbt_admin folder (`npm install`).
+make build-hdbt-admin # Builds SCSS/JS assets inside hdbt_admin folder (`npm run build`).
+make watch-hdbt-admin # Starts SCSS/JS watcher inside hdbt_admin folder (`npm run dev`).
+```
+
+See [/tools/make/project/theme.mk](/tools/make/project/theme.mk) for up-to-date list of available commands.
--- tools/make/project/theme.mk
+++ tools/make/project/theme.mk
@@ -1,41 +1,41 @@
 PHONY += install-hdbt-subtheme
-install-hdbt-subtheme:
+install-hdbt-subtheme: ## Installs dependencies for HDBT subtheme
 	$(call node,/app/public/themes/custom/hdbt_subtheme,"npm install")
 
 PHONY += build-hdbt-subtheme
-build-hdbt-subtheme:
+build-hdbt-subtheme: ## Builds SCSS/JS assets for HDBT subtheme
 	$(call node,/app/public/themes/custom/hdbt_subtheme,"npm run build")
 
 PHONY += watch-hdbt-subtheme
-watch-hdbt-subtheme:
+watch-hdbt-subtheme: ## Starts SCSS/JS watcher for HDBT subtheme
 	$(call node,/app/public/themes/custom/hdbt_subtheme,"npm run dev")
 
 PHONY += install-hdbt
-install-hdbt:
+install-hdbt: ## Installs dependencies for HDBT theme
 	$(call node,/app/public/themes/contrib/hdbt,"npm install")
 
 PHONY += build-hdbt
-build-hdbt:
+build-hdbt: ## Builds SCSS/JS assets for HDBT theme
 	$(call node,/app/public/themes/contrib/hdbt,"npm run build")
 
 PHONY += watch-hdbt
-watch-hdbt:
+watch-hdbt: ## Starts SCSS/JS watcher for HDBT theme
 	$(call node,/app/public/themes/contrib/hdbt,"npm run dev")
 
 PHONY += install-hdbt-admin
-install-hdbt-admin:
+install-hdbt-admin: ## Installs dependencies for HDBT Admin theme
 	$(call node,/app/public/themes/contrib/hdbt_admin,"npm install")
 
 PHONY += build-hdbt-admin
-build-hdbt-admin:
+build-hdbt-admin: ## Builds SCSS/JS assets for HDBT Admin theme
 	$(call node,/app/public/themes/contrib/hdbt_admin,"npm run build")
 
 PHONY += watch-hdbt-admin
-watch-hdbt-admin:
+watch-hdbt-admin: ## Starts SCSS/JS watcher for HDBT Admin theme
 	$(call node,/app/public/themes/contrib/hdbt_admin,"npm run dev")
 
-PHONY += node shell
-node-shell:
+PHONY += node-shell
+node-shell: ## Login to node container
 	$(call node,/app,sh)
 
 define node
