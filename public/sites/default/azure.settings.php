dfa9e3686f4a6e3a07e08e31b34b547831ee05fb
tuutti <tuutti@iki.fi>
2023-09-07 08:43:13 +0300

    Deployment identifier

--- public/sites/default/azure.settings.php
+++ public/sites/default/azure.settings.php
@@ -22,3 +22,14 @@ $config['filelog.settings']['location'] = '/tmp';
 $config['system.performance']['stale_file_threshold'] = 7776000;
 
 $settings['is_azure'] = TRUE;
+
+/**
+ * Deployment identifier.
+ *
+ * Use OpenShift build name (like 'drupal-1234') to determine
+ * if container needs to be invalidated and rebuilt.
+ *
+ * This should fix the issue where deployment fails due to changed service
+ * parameters.
+ */
+$settings['deployment_identifier'] = getenv('OPENSHIFT_BUILD_NAME');
