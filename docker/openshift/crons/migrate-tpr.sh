44156d946ae651a5dc49cc0a021bbc3c8b3d12a3
tuutti <tuutti@iki.fi>
2023-11-30 15:23:04 +0200

    UHF-8943: Copy init script, added comment

--- docker/openshift/crons/migrate-tpr.sh
+++ docker/openshift/crons/migrate-tpr.sh
@@ -2,6 +2,7 @@
 
 while true
 do
+  # Allow migrations to be run every 3 hours and reset stuck migrations every 12 hours.
   drush migrate:import tpr_unit --no-progress --reset-threshold 43200 --interval 10800
   drush migrate:import tpr_service --no-progress --reset-threshold 43200 --interval 10800
   drush migrate:import tpr_errand_service --no-progress --reset-threshold 43200 --interval 10800
