fa0b7df14c88a54b2a361ecfe6a80f956cdbfd38
tuutti <tuutti@iki.fi>
2023-12-01 09:31:02 +0200

    UHF-8943: Fixed sonarcloud

--- docker/openshift/preflight/preflight.php
+++ docker/openshift/preflight/preflight.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 /**
@@ -59,11 +60,11 @@ function environment_variable_isset(array $items) : bool {
 }
 
 $class_loader = require_once './../vendor/autoload.php';
-include_once './sites/default/settings.php';
+require_once './sites/default/settings.php'; // NOSONAR
 
 if (!isset($preflight_checks['environmentVariables'])) {
   echo 'No preflight checks found. Skipping ...';
-  exit(0); // NOSONAR
+  exit(0);
 }
 
 $candidates = [
@@ -81,7 +82,7 @@ foreach ($candidates as $candidate) {
   if (!file_exists($fileName)) {
     continue;
   }
-  require_once $fileName;
+  require_once $fileName; // NOSONAR
 }
 
 foreach ($preflight_checks['environmentVariables'] as $item) {
@@ -98,5 +99,5 @@ foreach ($preflight_checks['environmentVariables'] as $item) {
 // Fail deployment if any preflight check has failed.
 if ($messages = preflight_messages()) {
   echo implode(PHP_EOL, $messages);
-  exit(1); // NOSONAR
+  exit(1);
 }
