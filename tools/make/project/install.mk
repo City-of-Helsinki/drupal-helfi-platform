94bf276ee9eb4d30150a057e319d8c85a918dc4b
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2024-03-21 17:34:14 +0200

    Updated the order of translation import when running make fresh.

--- tools/make/project/install.mk
+++ tools/make/project/install.mk
@@ -1,9 +1,9 @@
 ifeq ($(DRUPAL_CONF_EXISTS),yes)
-	DRUPAL_NEW_TARGETS := up build drush-si drush-cr drush-locale-update drush-uli
+	DRUPAL_NEW_TARGETS := up build drush-si drush-cr drush-locale-update drush-helfi-locale-import drush-uli
 else
-	DRUPAL_NEW_TARGETS := up build drush-si helfi-drush-enable-modules drush-locale-update drush-uli
+	DRUPAL_NEW_TARGETS := up build drush-si helfi-drush-enable-modules drush-locale-update drush-helfi-locale-import drush-uli
 endif
-DRUPAL_POST_INSTALL_TARGETS := drush-deploy drush-locale-update drush-uli
+DRUPAL_POST_INSTALL_TARGETS := drush-locale-update drush-deploy drush-helfi-locale-import drush-uli
 
 OC_LOGIN_TOKEN ?= $(shell bash -c 'read -s -p "You must obtain an API token by visiting https://oauth-openshift.apps.arodevtest.hel.fi/oauth/token/request (Token):" token; echo $$token')
 
@@ -39,5 +39,9 @@ drush-locale-update: ## Update translations.
 	$(call drush,locale:check)
 	$(call drush,locale:update)
 	$(call drush,cr)
-	$(call step,Import custom translations...)
+
+PHONY += drush-helfi-locale-import
+drush-helfi-locale-import: ## Update translations from helfi platform config.
+	$(call step,Import helfi platform config translations...)
 	$(call drush,helfi:locale-import helfi_platform_config)
+	$(call drush,cr)
