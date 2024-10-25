2468df5287499233fe79caedd817b8cf74dbb7cc
tuutti <tuutti@iki.fi>
2024-10-25 09:26:26 +0300

    Update pubsub.sh

--- docker/openshift/crons/pubsub.sh
+++ docker/openshift/crons/pubsub.sh
@@ -9,7 +9,7 @@ echo "Running PubSub daemon: $(date +'%Y-%m-%dT%H:%M:%S%:z')"
 
 while true
 do
-  # PubSub process exists with success return code after
+  # PubSub process exits with success return code after
   # certain number of messages and should then be restarted.
   drush helfi:azure:pubsub-listen || exit 1
 done
