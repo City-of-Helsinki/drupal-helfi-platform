e6c29d9072cbb532848529d114fdca7898e3851d
tuutti <tuutti@iki.fi>
2025-01-21 09:51:14 +0200

    UHF-11255: Fixed typo

--- tools/make/project/init-project.mk
+++ tools/make/project/init-project.mk
@@ -1,6 +1,6 @@
 	PHONY += init-project
 	init-project:
-			@rm CHANGELOG.md README.md renovate.json .github/workflows/ci.yml tools/make/project/spell.mk
+			@rm CHANGELOG.md README.md renovate.json .github/workflows/ci.yml tools/make/project/init-project.mk
 			@rm -r documentation/
 			@mv README.project.md README.md
 			@composer config --unset scripts.post-create-project-cmd
