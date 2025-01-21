cebdec2204790a7c685b9d5c1580261bf11206c1
tuutti <tuutti@iki.fi>
2025-01-21 09:53:12 +0200

    UHF-11255: Fixed tabs

--- tools/make/project/init-project.mk
+++ tools/make/project/init-project.mk
@@ -1,7 +1,7 @@
-	PHONY += init-project
-	init-project:
-			@rm CHANGELOG.md README.md renovate.json .github/workflows/ci.yml tools/make/project/init-project.mk
-			@rm -r documentation/
-			@mv README.project.md README.md
-			@composer config --unset scripts.post-create-project-cmd
-			@git init && git add .
+PHONY += init-project
+init-project:
+	@rm CHANGELOG.md README.md renovate.json .github/workflows/ci.yml tools/make/project/init-project.mk
+	@rm -r documentation/
+	@mv README.project.md README.md
+	@composer config --unset scripts.post-create-project-cmd
+	@git init && git add .
