ffb14662599fee047e158088b5e4d818b22b26f1
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-06-05 14:23:37 +0300

    UHF-11784: Reverted part of commit 00c71d76b8fb3eaeafec2b715104013f5e75fae8.

--- docker/openshift/deploy/20-deploy.sh
+++ docker/openshift/deploy/20-deploy.sh
@@ -10,6 +10,10 @@ source /init.sh
 
 echo "Starting deploy: $(date)"
 
+# Populate deploy ID so 20-deploy.sh is skipped.
+# @todo Remove this once 20-deploy.sh is removed.
+set_deploy_id $OPENSHIFT_BUILD_NAME
+
 drush state:set system.maintenance_mode 1 --input-format=integer
 # Run pre-deploy tasks.
 # @see https://github.com/City-of-Helsinki/drupal-module-helfi-api-base/blob/main/documentation/deploy-hooks.md
